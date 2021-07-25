fd1928aea5a3b0f68fba0ff33ef8d38e5194d933
Andrei Vovchok <andreivovchok@bk.ru>
2021-07-25 13:57:15 +0300

    Добавил решение второго задания

---
+++ HomeWorkNumberThree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HomeWorkNumberThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 14, y = 1, z = 5;
+            x += y - x++ * z;
+            Console.WriteLine($"Переменная х ={x}, переменная y = {y}, переменная z = {z}");
+            z = --x - y * 5;
+            Console.WriteLine($"Переменная х ={x}, переменная y = {y}, переменная z = {z}");
+            y /= x + 5 % z;
+            Console.WriteLine($"Переменная х ={x}, переменная y = {y}, переменная z = {z}");
+            z = x++ + y * 5;
+            Console.WriteLine($"Переменная х ={x}, переменная y = {y}, переменная z = {z}");
+            x = y - x++ * z;
+            Console.WriteLine($"Переменная х ={x}, переменная y = {y}, переменная z = {z}");
+
+        }
+    }
+}
